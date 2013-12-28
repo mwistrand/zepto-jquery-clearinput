136f7c87c752015532af8a04e32654c9197a705f
Matt Wistrand <matt@troglodytidae.com>
2013-12-28 14:59:37 -0500

    Added complete plugin code and tests.

--- src/jquery.clearInput.js
+++ src/jquery.clearInput.js
@@ -1,8 +1,36 @@
 (function($) {
 'use strict';
 
-$.fn.clearInput = function() {
+$.fn.clearInput = function(delegate, focusedClass) {
+  return this.each(function(i) {
+    var $el = $(this);
+    
+    delegate || (delegate = this.nodeName === 'INPUT' ? null : 'input');
+    focusedClass || (focusedClass = 'is-focused');
+    
+    $el.on({
+      focus: function(e) {
+        var $input = $(e.currentTarget),
+          value = $input.val();
 
+        if (value !== '') {
+          $input.data('initial-value', value);
+        }
+
+        $input.addClass(focusedClass).val('');
+      },
+
+      blur: function(e) {
+        var $input = $(e.currentTarget);
+
+        $input.removeClass(focusedClass);
+
+        if (!$input.val()) {
+          $input.val($input.data('initial-value') || '');
+        }
+      }
+    }, delegate);
+  });
 };
-
+  
 })(window.Zepto || window.jQuery);
--- test/spec/jquery.clearInput.spec.js
+++ test/spec/jquery.clearInput.spec.js
@@ -1,3 +1,62 @@
 describe('$ plugin to clear input values', function() {
   'use strict';
+
+  var $input,
+    $container;
+
+  beforeEach(function() {
+    loadFixtures('inputs.html');
+
+    $container = $('.container');
+    $input = $('input');
+  });
+  afterEach(function() {
+    $container.empty().remove();
+  });
+
+  it('stores the initial of an input on focus', function() {
+    $container.clearInput();
+    $input.focus();
+
+    expect($input.data('initial-value')).toEqual('Lorem ipsum');
+  });
+
+  it('restores the original value on blur', function() {
+    $container.clearInput();
+    $input.focus().blur();
+
+    expect($input.val()).toEqual('Lorem ipsum');
+  });
+
+  it('does not restore the original value if a new value is entered',
+      function() {
+    $container.clearInput();
+    $input.focus().val('New value').blur();
+
+    expect($input.val()).toEqual('New value');
+  });
+
+  it('can be called directly on HTML input elements', function() {
+    $input.clearInput().focus();
+
+    expect($input.val()).toEqual('');
+  });
+
+  it('can be passed a delegate selector', function() {
+    $container.clearInput('.js-clear');
+    $input.focus();
+    expect($input.val()).toEqual('');
+  });
+
+  it('adds the specified class on focus', function() {
+    $container.clearInput('input', 'focused');
+    $input.focus();
+    expect($input).toHaveClass('focused');
+  });
+
+  it('removes the specified class on blur', function() {
+    $container.clearInput('input', 'focused');
+    $input.focus().blur();
+    expect($input).not.toHaveClass('focused');
+  });
 });
